cb537fc58c3a5e24182609e67cb5b5585e9f9f83
Arunkumar M <arunkumarsm1@gmail.com>
2018-10-20 22:53:15 +0530

    R code file

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,6 @@
 
 # Getting and Cleaning Data Course Project Getting and Cleaning Data Course Project
 ###################################################################################
-
 # Link to description of data from site: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 # Data for the project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 ##################################################################################################
